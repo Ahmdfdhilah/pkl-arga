fae933ddad87b3b8bb7f446298699b00b636c29a
Ahmdfdhilah <ahmad.121140173@student.itera.ac.id>
2025-06-18 01:22:51 +0700

    init

--- sistem/koneksi.php
+++ sistem/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 function open_connection() {
-    $hostname = "b7ugrctah5ilqvtdwgwg-mysql.services.clever-cloud.com";
-    $username = "un4hveqvjpr3ypef";
-    $password = "mDmLTE1aGac8xpVwwnMH"; // Sesuaikan dengan password database MySQL Anda
-    $dbname = "b7ugrctah5ilqvtdwgwg"; // Sesuaikan nama database Anda
+    $hostname = "localhost";
+    $username = "root";
+    $password = ""; // Sesuaikan dengan password database MySQL Anda
+    $dbname = "argapkl"; // Sesuaikan nama database Anda
 
     $koneksi = mysqli_connect($hostname, $username, $password, $dbname);
 
